3c83be129112bb15db45a4fc315e2f5e8a37cb2c
Wilk Maia <wilkmaia@gmail.com>
2020-12-02 13:40:02 -0300

    Add day 2 solution

---
+++ day2/src/main.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+struct PasswordRule {
+    min: u32,
+    max: u32,
+    letter: char,
+}
+
+struct Password {
+    rule: PasswordRule,
+    password: String,
+}
+
+fn get_password(line: &str) -> Password {
+    let min_str = line.split('-').collect::<Vec<&str>>()[0];
+    let max_str = line.split('-').collect::<Vec<&str>>()[1].split(' ').collect::<Vec<&str>>()[0];
+    let letter = String::from(line.split(':').collect::<Vec<&str>>()[0]).pop().unwrap();
+    let password = String::from(line.split(": ").collect::<Vec<&str>>()[1]);
+
+    Password {
+        rule: PasswordRule {
+            min: min_str.parse::<u32>().unwrap(),
+            max: max_str.parse::<u32>().unwrap(),
+            letter,
+        },
+        password,
+    }
+}
+
+fn initialize_array(arr: &mut Vec<Password>) {
+    let mut file = File::open("./input").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    for line in contents.split('\n') {
+        arr.push(get_password(line));
+    }
+}
+
+fn problem1(passwords: &Vec<Password>) {
+    let mut valid_count = 0u32;
+
+    for password in passwords {
+        let mut letter_count = 0u32;
+
+        for c in password.password.as_str().chars() {
+            if c == password.rule.letter {
+                letter_count += 1;
+            }
+        }
+
+        if letter_count >= password.rule.min && letter_count <= password.rule.max {
+            valid_count += 1;
+        }
+    }
+
+    println!("Problem 1 - Valid Count = {}", valid_count);
+}
+
+fn problem2(passwords: &Vec<Password>) {
+    let mut valid_count = 0u32;
+
+    for password in passwords {
+        let mut letter_count = 0u32;
+        let chars = password.password.as_bytes();
+
+        if chars[(password.rule.min - 1) as usize] as char == password.rule.letter {
+            letter_count += 1
+        }
+
+        if chars[(password.rule.max - 1) as usize] as char == password.rule.letter {
+            letter_count += 1
+        }
+
+        if letter_count == 1 {
+            valid_count += 1;
+        }
+    }
+
+    println!("Problem 2 - Valid Count = {}", valid_count);
+}
+
+fn main() {
+    let mut passwords = Vec::<Password>::new();
+    initialize_array(&mut passwords);
+
+    problem1(&passwords);
+    problem2(&passwords);
+}
