29d8e401b0b8a133b95fa4a01fed45adaee62ed5
Wilk Maia <wilkmaia@gmail.com>
2020-12-05 23:06:42 -0300

    Add day 5 solution

---
+++ day5/src/main.rs
@@ -0,0 +1,92 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashSet;
+
+#[derive(Debug)]
+struct BoardingPass {
+    row: u8,
+    col: u8,
+    id: u16,
+}
+
+fn parse_boarding_pass(line: &str) -> BoardingPass {
+    let (row_data, col_data) = line.split_at(7);
+
+    let (row, _) = row_data.chars().fold((0, 127), |acc, c| -> (u8, u8) {
+        match c {
+            'F' => (acc.0, (acc.0 + acc.1)/2),
+            'B' => ((acc.0 + acc.1)/2 + 1, acc.1),
+            _ => (0, 0),
+        }
+    });
+
+    let (col, _) = col_data.chars().fold((0, 7), |acc, c| -> (u8, u8) {
+        match c {
+            'L' => (acc.0, (acc.0 + acc.1)/2),
+            'R' => ((acc.0 + acc.1)/2 + 1, acc.1),
+            _ => (0, 0),
+        }
+    });
+
+    BoardingPass {
+        row,
+        col,
+        id: 8 * (row as u16) + col as u16,
+    }
+}
+
+fn initialize_array(arr: &mut Vec<BoardingPass>) {
+    let mut file = File::open("./input").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    for line in contents.split('\n') {
+        arr.push(parse_boarding_pass(line));
+    }
+}
+
+fn problem1(boarding_passes: &Vec<BoardingPass>) {
+    let max = boarding_passes.iter().fold(0, |acc, b| -> u16 {
+        if b.id > acc {
+            return b.id
+        } else {
+            acc
+        }
+    });
+
+    println!("Problem 1 -> {}", max);
+}
+
+fn problem2(boarding_passes: &Vec<BoardingPass>) {
+    let mut seats = HashSet::new();
+
+    let max = boarding_passes.iter().fold(0, |acc, b| {
+        seats.insert(b.id);
+
+        if b.id > acc {
+            return b.id
+        } else {
+            acc
+        }
+    });
+
+    let mut seat: u16 = 0;
+
+    for i in 1..(max - 2) {
+        if seats.contains(&(i - 1)) && seats.contains(&(i + 1)) && !seats.contains(&i) {
+            seat = i;
+            break;
+        }
+    }
+
+    println!("Problem 2 -> {}", seat);
+}
+
+fn main() {
+    let mut boarding_passes = Vec::<BoardingPass>::new();
+    initialize_array(&mut boarding_passes);
+
+    problem1(&boarding_passes);
+    problem2(&boarding_passes);
+}
