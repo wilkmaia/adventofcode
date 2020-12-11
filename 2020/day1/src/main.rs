1fe55deadf422603c505ceabf67bbb9712d1a81f
Wilk Maia <wilkmaia@gmail.com>
2020-12-10 21:56:59 -0300

    Improve day 1 solution

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -3,70 +3,60 @@ extern crate utils;
 use utils::parse_input;
 use utils::basic_parser;
 
-fn problem2(data: &Vec<i32>) -> Option<i32> {
-    let mut i = 0usize;
-    while i < data.len(){
+const TARGET: i32 = 2020;
+
+fn problem1(data: &Vec<i32>) {
+    let (mut i, mut j) = (0, data.len() - 1);
+
+    while i != j {
         let n1 = data[i];
-        let mut j = i + 1usize;
+        let n2 = data[j];
 
-        while j < data.len() {
-            let n2 = data[j];
-            if n1 + n2 > 2020 {
-                break;
-            }
-
-            let mut k = j + 1usize;
-            while k < data.len() {
-                let n3 = data[k];
-                if n3 > 2020 {
-                    break;
-                }
-
-                if n1 + n2 + n3 == 2020 {
-                    return Some(n1 * n2 * n3);
-                }
-
-                k += 1;
-            }
-
-            j += 1;
+        if n1 + n2 == TARGET {
+            println!("Problem 1 -> {}", n1 * n2);
+            return
+        } else if n1 + n2 > TARGET {
+            j -= 1;
+        } else {
+            i += 1;
         }
-
-        i += 1;
     }
 
-    return None;
+    println!("No solution for problem 1 with target = {}", TARGET);
 }
 
-fn problem1(data: &Vec<i32>) -> Option<i32> {
-    let mut i = 0usize;
-    while i < data.len(){
-        let n1 = data[i];
-        let mut j = i + 1usize;
+fn problem2(data: &Vec<i32>) {
+    match data.iter()
+        .enumerate()
+        .find(|(idx, &n)| -> bool {
+            let (mut i, mut j) = (idx + 1, data.len() - 1);
 
-        while j < data.len() {
-            let n2 = data[j];
-            if n2 > 2020 {
-                break;
+            while i != j {
+                let n1 = data[i];
+                let n2 = data[j];
+
+                if n + n1 + n2 == TARGET {
+                    println!("Problem 2 -> {}", n * n1 * n2);
+                    return true
+                } else if n + n1 + n2 > TARGET {
+                    j -= 1;
+                } else {
+                    i += 1;
+                }
             }
 
-            if n1 + n2 == 2020 {
-                return Some(n1 * n2);
-            }
-
-            j += 1;
-        }
-
-        i += 1;
-    }
-
-    return None;
+            return false
+        })
+    {
+        Some(_) => (),
+        None => println!("No solution for problem 2 with target = {}", TARGET)
+    };
 }
 
 fn main() {
     let mut data = parse_input::<i32>("input", "\n", basic_parser::<i32>);
     data.sort();
 
-    println!("Problem 1 -> {}", problem1(&data).unwrap());
-    println!("Problem 2 -> {}", problem2(&data).unwrap());
+    problem1(&data);
+    problem2(&data);
 }
